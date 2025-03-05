b7e210a67c4efc461ef72b371e7414b4d2f5dfb1
Cristian Polisi <cristian.polisi2002@gmail.com>
2025-03-05 13:19:22 +0100

    COmpletamento progetto pt.1

--- main.cpp
+++ main.cpp
@@ -10,12 +10,12 @@
 using namespace std;
 
 int main() {
-    ContoCorrente conto1("IT60X0542811101000000123456", "Mario Rossi", "transazioni.csv");
+    ContoCorrente conto1("IT60X0542811101000000123456", "Mario Rossi", "../transazioni.csv");
 
-    ContoCorrente conto2("IT80Y0326812311000000987654", "Luigi Bianchi", "transazioni.csv");
+    ContoCorrente conto2("IT80Y0326812311000000987654", "Luigi Bianchi", "../transazioni.csv");
 
 
-    ContoCorrente contoCaricato1("IT60X0542811101000000123456", "Mario Rossi", "transazioni.csv");
+    ContoCorrente contoCaricato1("IT60X0542811101000000123456", "Mario Rossi", "../transazioni.csv");
     contoCaricato1.caricaDaFile();
     contoCaricato1.stampaTransazioni();
 
@@ -23,7 +23,7 @@ int main() {
     cout << " " << endl;
     cout << " " << endl;
 
-    ContoCorrente contoCaricato2("IT80Y0326812311000000987654", "Luigi Bianchi", "transazioni.csv");
+    ContoCorrente contoCaricato2("IT80Y0326812311000000987654", "Luigi Bianchi", "../transazioni.csv");
     contoCaricato2.caricaDaFile();
     contoCaricato2.stampaTransazioni();
  return 0;
